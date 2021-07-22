89c96671f7cd5cf99de243dcbdce721912d8313d
Priiiyaa <ps4389196@gmail.com>
2021-07-22 18:56:53 +0545

    update

---
+++ introduction.py
@@ -0,0 +1,29 @@
+#print("hello")
+#print('mother\'s')
+
+
+# print("hello are you okay or not")
+#
+# print(12345)
+# hello='priya'
+# hi='pandey'
+# print (F"{hello} {hi}")
+# print ('priya','pandey' ,'nepal' , ('25'))
+#
+# Name = "Priya"
+# Age = 18
+# Phone= 9806988875
+# print (f"My name is {Name} {Age} {Phone}")
+#
+# name=input ("Enter name:")
+# age= float(input ("Enter age:"))
+# phone= int (input ("Enter phone:"))
+# print(f"My name: {name} Age: {age} Phone:{phone}")
+
+# print('mother\'s')
+# print("mother's")
+
+
+# Sp=int(input("enter s.p:"))
+# Cp=int(input("enter c.p:"))
+# print({(Sp-Cp)/100})
